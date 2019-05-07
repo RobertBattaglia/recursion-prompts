931cf26cbdcd9d802987f15be049fcd83d628eae
Robert Battaglia <r083rt164@gmail.com>
2019-05-07 14:18:50 -0400

    Completed Sum of Integers

--- src/recursion.js
+++ src/recursion.js
@@ -23,6 +23,18 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	if (!array.length) {
+		return 0;
+	}
+
+	if (array.length === 1) {
+		return array[0];
+	}
+
+	var copyArray = array.slice();
+	var firstElement = copyArray.shift();
+	copyArray[0] += firstElement;
+	return sum(copyArray);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
